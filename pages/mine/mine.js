6cca583687b26efaa1f7930b271da718df8691d5
YZJ <1907589370@qq.com>
2019-12-25 09:07:53 +0800

    主分支

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,11 +1,15 @@
 // pages/mine/mine.js
+<<<<<<< HEAD
 const app = getApp()
+=======
+>>>>>>> books
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+<<<<<<< HEAD
     pickurl: '',
     nickname: '',
     country: '',
@@ -34,11 +38,15 @@ Page({
       title: '我的评论',
       icon: '>'
     }, ]
+=======
+
+>>>>>>> books
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+<<<<<<< HEAD
   onLoad: function() {
     this.setData({
       pickurl: app.globalData.avatarUrl,
@@ -47,54 +55,86 @@ Page({
       province: app.globalData.province,
       city: app.globalData.city
     })
+=======
+  onLoad: function (options) {
+
+>>>>>>> books
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
+<<<<<<< HEAD
   onReady: function() {
+=======
+  onReady: function () {
+>>>>>>> books
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
+<<<<<<< HEAD
   onShow: function() {
+=======
+  onShow: function () {
+>>>>>>> books
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
+<<<<<<< HEAD
   onHide: function() {
+=======
+  onHide: function () {
+>>>>>>> books
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
+<<<<<<< HEAD
   onUnload: function() {
+=======
+  onUnload: function () {
+>>>>>>> books
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
+<<<<<<< HEAD
   onPullDownRefresh: function() {
+=======
+  onPullDownRefresh: function () {
+>>>>>>> books
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
+<<<<<<< HEAD
   onReachBottom: function() {
+=======
+  onReachBottom: function () {
+>>>>>>> books
 
   },
 
   /**
    * 用户点击右上角分享
    */
+<<<<<<< HEAD
   onShareAppMessage: function() {
+=======
+  onShareAppMessage: function () {
+>>>>>>> books
 
   }
 })
